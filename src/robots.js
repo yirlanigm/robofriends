4bf49596dca9faade4142b92f73c86e1527aa151
yirlanigm <yirgm0704@gmail.com>
2020-12-29 11:06:02 -0600

    commit inicial

---
+++ src/robots.js
@@ -0,0 +1,59 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Anton',
+        email: 'Shanna@melissa.tv'
+    },
+
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+    },
+
+    {
+        id: 6,
+        name: 'Mr. Dennis Schulist',
+        username: 'Leopoldo_Corkey',
+        email: 'Karley_Dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Noriah.Stanton',
+        email: 'Rey.Padberg@karina.biz'
+    }
+]
